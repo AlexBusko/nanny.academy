b77a0dc2c6282f505bae5b17ddcfb05534a6da3c
Alex Busko <alexbusko@icloud.com>
2021-07-09 14:52:29 +0300

    countdown created

--- src/pages/events/FirstEvent/FirstEvent.js
+++ src/pages/events/FirstEvent/FirstEvent.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Countdown from "../../../components/countdown/Countdown";
 import "./style.scss";
 
 const FirstEvent = () => {
@@ -8,6 +9,7 @@ const FirstEvent = () => {
       <div className="event-page">
         <Container>
           <h1>Наш перший івент</h1>
+          <Countdown date={`07/30/2021/19:00`} message={"Подія відбулась"}/>
         </Container>
       </div>
       <div className="event-page-back"></div>
